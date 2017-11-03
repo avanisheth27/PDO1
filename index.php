8ced2369b8d4c0628101153a3a20e8edcdb289a0
Avani V Sheth <avs44@afsaccess1.njit.edu>
2017-11-03 15:24:12 -0400

    added

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "sql2.njit.edu";
+$username = "avs44";
+$password = "APcA7GqK";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
