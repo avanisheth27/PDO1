20a323fde6d080c56f91f904ab71040bab43460b
Avani V Sheth <avs44@afsconnect2.njit.edu>
2017-11-04 09:54:22 -0400

    added

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<!Doctype HTML>
+
 <html>
 <body>
-	<?php
+
+<?php
 
 $servername = "sql2.njit.edu";
 $username = "avs44";
 $password = "APcA7GqK";
+$query="SELECT * from accounts";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -17,16 +19,30 @@ if ($conn->connect_error) {
 echo "<b>Connected successfully</b>";
 echo "<br><hr>";
 
-$sql = "SELECT * from accounts";
+function runQuery($query) {
+ 	global $conn;
+     try {
+ 		$q = $conn->prepare($query);
+ 		$q->execute();
+ 	$results = $q->fetchAll();
+ 	$q->closeCursor();
+ 		return $results;	
+ 	} catch (PDOException $e) {
+ 		http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n" . $e->getMessage());
+ 	}	  
+ }
+
+/*$sql = "SELECT * from accounts";
 $result = $conn->query($sql);
 
 try {
   
 $conn->exec($sql);
 } catch (PDOException $e) {
-echo $result1 . "<br>" . $e->getMessage();
+echo $result1 . "<br>" ;
 
-}
+}*/
+
 echo 'done';
 $conn->close();
 ?>
