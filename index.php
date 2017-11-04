21c0a360adb375fa6d6c80c350b3f69c90071a19
Avani V Sheth <avs44@afsconnect2.njit.edu>
2017-11-04 09:39:48 -0400

    added

--- index.php
+++ index.php
@@ -16,6 +16,19 @@ if ($conn->connect_error) {
 } 
 echo "<b>Connected successfully</b>";
 echo "<br><hr>";
+
+$sql = "SELECT * from accounts";
+$result = $conn->query($sql);
+
+try {
+  
+$conn->exec($sql);
+} catch (PDOException $e) {
+echo $result1 . "<br>" . $e->getMessage();
+
+}
+echo 'done';
+$conn->close();
 ?>
 </body>
 </html>
