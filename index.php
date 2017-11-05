0c6fefad29b5e8bf56f09a7cbefadfe5a7b9f35e
Avani V Sheth <avs44@afsconnect2.njit.edu>
2017-11-05 17:15:34 -0500

    added

--- index.php
+++ index.php
@@ -2,30 +2,36 @@
 $dsn = 'mysql:dbname=avs44;host=sql2.njit.edu';
 $user = 'avs44';
 $password = 'APcA7GqK';
-
-try 
+ 
+try
 {
     $dbh = new PDO($dsn, $user, $password);
     echo "<b>Connected successfully</b>";
     echo "<br><hr>";
-
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} 
-
-catch (PDOException $e) 
+}
+ 
+catch (PDOException $e)
 {
   echo 'Connection failed: ' . $e->getMessage();
 }
-
-$sql = "Select * from accounts";
-
-try 
-{
-  $dbh->exec($sql);
-} 
-catch (PDOException $e) 
-{
-echo $sql . "<br>" . $e->getMessage();
+try {
+    $stmt = $dbh->prepare("SELECT * FROM accounts where id<6");
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $stmt->fetchAll();
+    //print_r($result);
+    
+    }
+    
+catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
 }
-echo 'done';
-?>
+echo '<b>Number of Records returned from the Accounts table are :</b> ' ;
+Print_r(count($result));
+echo '<br><hr>';
+
+   //Print_r($result);
+
+ 
+ ?>
