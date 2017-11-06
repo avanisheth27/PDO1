80d5a7902afc77d3a79db5f6197b267b2b1bd19a
Avani V Sheth <avs44@afsconnect2.njit.edu>
2017-11-05 20:49:06 -0500

    added

--- index.php
+++ index.php
@@ -30,8 +30,29 @@ catch (PDOException $e) {
 echo '<b>Number of Records returned from the Accounts table are :</b> ' ;
 Print_r(count($result));
 echo '<br><hr>';
+echo '<br>';
+
+
 
    //Print_r($result);
+  echo "<table cellpadding='5px' border='1px' style='border-collapse: collapse'>";
+ echo '<tr><th>id</th><th>email</th><th>fname</th><th>lname</th><th>phone</th><th>birthday</th><th>gender</th><th>password</th></tr>';
+foreach ($result as $label)
+{
+    echo '<tr>';
+       
+        echo '<td>'.$label['id'].'</td>';
+        echo '<td>'.$label['email'].'</td>';
+        echo '<td>'.$label['fname'].'</td>';
+        echo '<td>'.$label['lname'].'</td>';
+        echo '<td>'.$label['phone'].'</td>';
+        echo '<td>'.$label['birthday'].'</td>';
+        echo '<td>'.$label['gender'].'</td>';
+        echo '<td>'.$label['password'].'</td>';
+        
+    echo '</tr>';
+}
+echo '</table>';
 
 
  
